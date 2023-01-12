fa4009d8c56812dca0a7c76645e310fb0d773bd0
Madeleine <madelea@amazon.com>
2023-01-11 16:52:25 -0800

    Forgot to save

--- tls/s2n_server_hello.c
+++ tls/s2n_server_hello.c
@@ -270,7 +270,6 @@ int s2n_server_hello_recv(struct s2n_connection *conn)
     POSIX_GUARD(s2n_server_hello_parse(conn));
 
     conn->actual_protocol_version_established = 1;
-    POSIX_GUARD_RESULT(s2n_conn_choose_state_machine(conn, conn->actual_protocol_version));
 
     POSIX_GUARD(s2n_conn_set_handshake_type(conn));
 
